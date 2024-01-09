f02220b0a16e66c61560ba6899e8506ddd1cb566
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2024-01-10 01:13:29 +0600

    endpoint for person count

---
+++ app.py
@@ -0,0 +1,44 @@
+import base64
+import cv2
+from flask import Flask, request, jsonify
+from src.main import Prediction
+
+app = Flask(__name__)
+
+
+# defining an endpoint for running inference on image
+@app.route('/predict', methods=["POST"])
+def predict():
+    dict_to_return = {"person_count": "Exception Occurred"}
+    try:
+        json_data = request.get_json(force=True)  # getting the json data
+        # getting the image data in base64 format
+        image_data = json_data['image']
+
+        # converting the base64 image data into bytes
+        image = base64.b64decode(image_data, validate=True)
+
+        # saving the image
+        with open("saved_image.jpg", 'wb') as f:
+            f.write(image)
+            print("Image saved successfully\n")
+
+            # reading the saved image
+            frame = cv2.imread("saved_image.jpg", cv2.IMREAD_COLOR)
+
+            # prediction object
+            prediction_object = Prediction()
+
+            # passing the image to yolov8 model
+            person_count = prediction_object.count_person(frame)
+            print(f"Number of percent count is: {person_count}")
+
+            # returning the response
+            dict_to_return['person_count'] = person_count
+    except Exception as e:
+        print(f"Exception: {e}")
+
+    return jsonify(dict_to_return)
+
+
+app.run()
--- src/main.py
+++ src/main.py
@@ -5,16 +5,14 @@ from ultralytics import YOLO
 model = YOLO('/src/model/yolov8x.pt')
 
 
-def count_from_image(frame):
-    # Run YOLOv8 inference on the frame
-    results = model.predict(source=frame, conf=0.5,
-                            iou=0.6, classes=[0], imgsz=[640, 480])
-    # Get the count of persons on the frame
-    person_count = len(results[0])
-    return person_count
+class Prediction:
+    def __init__(self):
+        self.model_path = "src/model/yolov8.pt"
 
-
-if __name__ == "__main__":
-    frame = cv2.imread("image.jpg", cv2.IMREAD_COLOR)
-    print(f"Number of percent count is: {count_from_image(frame)}")
-    cv2.destroyAllWindows()
+    def count_person(self, frame):
+        # Run YOLOv8 inference on the frame
+        results = model.predict(source=frame, conf=0.5,
+                                iou=0.6, classes=[0], imgsz=[640, 480])
+        # Get the count of persons on the frame
+        person_count = len(results[0])
+        return person_count
